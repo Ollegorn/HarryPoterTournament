9ab4514ec3b94020ce8b4aab18f2d0552a90abae
John Kakouris <jdkakouris@gmail.com>
2023-09-01 13:20:55 +0300

    initial migration db

---
+++ Entities/User.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace Entities
+{
+    public class User : IdentityUser
+    {
+        public string DisplayName { get; set; }
+
+        public int CurrentTournamentPoints { get; set; }
+    }
+}
--- HarryPotterTournament/Program.cs
+++ HarryPotterTournament/Program.cs
@@ -1,9 +1,22 @@
+using Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<User, IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
